b30f750f1ffe7b708a18404bc8a7085469135e4f
Artem Smaznov <31800376+ArtemSmaznov@users.noreply.github.com>
2020-05-31 22:27:59 +0100

    awesome/segregates system tray widget

--- config/awesome/floppy/widget/system-tray.lua
+++ config/awesome/floppy/widget/system-tray.lua
@@ -2,16 +2,29 @@ local beautiful = require('beautiful')
 local wibox = require('wibox')
 local dpi = beautiful.xresources.apply_dpi
 
-local sys_tray = wibox.widget {
-	{
-		visible = true,
-		base_size = dpi(30),
-		horizontal = false,
-		screen = 'primary',
-		widget = wibox.widget.systray
-	},
-	margins = dpi(5),
-	widget = wibox.container.margin
-}
 
-return sys_tray
+return function(s, action_bar_width)
+	
+	s.systray = wibox.widget {
+		{
+			visible = true,
+			base_size = dpi(30),
+			horizontal = false,
+			screen = 'primary',
+			widget = wibox.widget.systray
+		},
+		margins = dpi(5),
+		widget = wibox.container.margin
+	}
+
+	s.tray_toggler  = require('widget.tray-toggler')
+
+	return wibox.widget {
+		id = 'system_tray',
+		layout = wibox.layout.align.vertical,
+		forced_width = action_bar_width,
+		s.tray_toggler,
+		nil,
+		s.systray
+	}
+end
