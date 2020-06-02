746aca05ad9c1b95765e532094e63370e91b4c34
Artem Smaznov <31800376+ArtemSmaznov@users.noreply.github.com>
2020-06-02 23:29:01 +0100

    awesome/added file check to screenshot module

--- config/awesome/floppy/configuration/client/keys.lua
+++ config/awesome/floppy/configuration/client/keys.lua
@@ -121,6 +121,27 @@ local clientKeys =
 	-- Screen management
 	awful.key(
 		{modkey, 'Shift'},
+		'Up',
+		function(c)
+			-- if client.focus then
+			-- 	local tag = client.focus.screen.tags[1]
+			-- 	if tag then
+			-- 			client.focus:move_to_tag(tag)
+			-- 	end
+			-- end
+		end,
+		{description = 'move window to previous tag', group = 'client'}
+	),
+	awful.key(
+		{modkey, 'Shift'},
+		'Down',
+		function(c)
+
+		end,
+		{description = 'move window to next tag', group = 'client'}
+	),
+	awful.key(
+		{modkey, 'Shift'},
 		'Left',
 		function(c)
 			c:move_to_screen(2)
@@ -142,140 +163,141 @@ local clientKeys =
 			c:move_to_screen()
 		end,
 		{description = 'move window to different screen', group = 'client'}
-	),
+	)
+
 	-- move client position
-	awful.key(
-		{modkey},
-		'Up',
-		function(c)
-			if c.floating then
-				c:relative_move(0, dpi(-10), 0, 0)
-			end
-		end,
-		{description = 'move floating client up by 10 px', group = 'client'}
-	),
-	awful.key(
-		{modkey},
-		'Down',
-		function(c)
-			if c.floating then
-				c:relative_move(0, dpi(10), 0, 0)
-			end
-		end,
-		{description = 'move floating client down by 10 px', group = 'client'}
-	),
-	awful.key(
-		{modkey},
-		'Left',
-		function(c)
-			if c.floating then
-				c:relative_move(dpi(-10), 0, 0, 0)
-			end
-		end,
-		{description = 'move floating client to the left by 10 px', group = 'client'}
-	),
-	awful.key(
-		{modkey},
-		'Right',
-		function(c)
-			if c.floating then
-				c:relative_move(dpi(10), 0, 0, 0)
-			end
-		end,
-		{description = 'move floating client to the right by 10 px', group = 'client'}
-	),
+	-- awful.key(
+	-- 	{modkey},
+	-- 	'Up',
+	-- 	function(c)
+	-- 		if c.floating then
+	-- 			c:relative_move(0, dpi(-10), 0, 0)
+	-- 		end
+	-- 	end,
+	-- 	{description = 'move floating client up by 10 px', group = 'client'}
+	-- ),
+	-- awful.key(
+	-- 	{modkey},
+	-- 	'Down',
+	-- 	function(c)
+	-- 		if c.floating then
+	-- 			c:relative_move(0, dpi(10), 0, 0)
+	-- 		end
+	-- 	end,
+	-- 	{description = 'move floating client down by 10 px', group = 'client'}
+	-- ),
+	-- awful.key(
+	-- 	{modkey},
+	-- 	'Left',
+	-- 	function(c)
+	-- 		if c.floating then
+	-- 			c:relative_move(dpi(-10), 0, 0, 0)
+	-- 		end
+	-- 	end,
+	-- 	{description = 'move floating client to the left by 10 px', group = 'client'}
+	-- ),
+	-- awful.key(
+	-- 	{modkey},
+	-- 	'Right',
+	-- 	function(c)
+	-- 		if c.floating then
+	-- 			c:relative_move(dpi(10), 0, 0, 0)
+	-- 		end
+	-- 	end,
+	-- 	{description = 'move floating client to the right by 10 px', group = 'client'}
+	-- ),
 
 	-- Increasing floating client size
-	awful.key(
-		{modkey, 'Shift'},
-		'Up',
-		function(c)
-			if c.floating then
-				c:relative_move(0, dpi(-10), 0, dpi(10))
-			end
-		end,
-		{description = 'increase floating client size vertically by 10 px up', group = 'client'}
-	),
-	awful.key(
-		{modkey, 'Shift'},
-		'Down',
-		function(c)
-			if c.floating then
-				c:relative_move(0, 0, 0, dpi(10))
-			end
-		end,
-		{description = 'increase floating client size vertically by 10 px down', group = 'client'}
-	),
-	awful.key(
-		{modkey, 'Shift'},
-		'Left',
-		function(c)
-			if c.floating then
-				c:relative_move(dpi(-10), 0, dpi(10), 0)
-			end
-		end,
-		{description = 'increase floating client size horizontally by 10 px left', group = 'client'}
-	),
-	awful.key(
-		{modkey, 'Shift'},
-		'Right',
-		function(c)
-			if c.floating then
-				c:relative_move(0, 0, dpi(10), 0)
-			end
-		end,
-		{description = 'increase floating client size horizontally by 10 px right', group = 'client'}
-	),
+	-- awful.key(
+	-- 	{modkey, 'Shift'},
+	-- 	'Up',
+	-- 	function(c)
+	-- 		if c.floating then
+	-- 			c:relative_move(0, dpi(-10), 0, dpi(10))
+	-- 		end
+	-- 	end,
+	-- 	{description = 'increase floating client size vertically by 10 px up', group = 'client'}
+	-- ),
+	-- awful.key(
+	-- 	{modkey, 'Shift'},
+	-- 	'Down',
+	-- 	function(c)
+	-- 		if c.floating then
+	-- 			c:relative_move(0, 0, 0, dpi(10))
+	-- 		end
+	-- 	end,
+	-- 	{description = 'increase floating client size vertically by 10 px down', group = 'client'}
+	-- ),
+	-- awful.key(
+	-- 	{modkey, 'Shift'},
+	-- 	'Left',
+	-- 	function(c)
+	-- 		if c.floating then
+	-- 			c:relative_move(dpi(-10), 0, dpi(10), 0)
+	-- 		end
+	-- 	end,
+	-- 	{description = 'increase floating client size horizontally by 10 px left', group = 'client'}
+	-- ),
+	-- awful.key(
+	-- 	{modkey, 'Shift'},
+	-- 	'Right',
+	-- 	function(c)
+	-- 		if c.floating then
+	-- 			c:relative_move(0, 0, dpi(10), 0)
+	-- 		end
+	-- 	end,
+	-- 	{description = 'increase floating client size horizontally by 10 px right', group = 'client'}
+	-- ),
 
 	-- Decreasing floating client size
-	awful.key(
-		{modkey, 'Control'},
-		'Up',
-		function(c)
-			if c.floating and c.height > 10 then
-				c:relative_move(0, 0, 0, dpi(-10))
-			end
-		end,
-		{description = 'decrease floating client size vertically by 10 px up', group = 'client'}
-	),
-	awful.key(
-		{modkey, 'Control'},
-		'Down',
-		function(c)
-			if c.floating then
-				local c_height = c.height
-				c:relative_move(0, 0, 0, dpi(-10))
-				if c.height ~= c_height and c.height > 10 then
-					c:relative_move(0, dpi(10), 0, 0)
-				end
-			end
-		end,
-		{description = 'decrease floating client size vertically by 10 px down', group = 'client'}
-	),
-	awful.key(
-		{modkey, 'Control'},
-		'Left',
-		function(c)
-			if c.floating and c.width > 10 then
-				c:relative_move(0, 0, dpi(-10), 0)
-			end
-		end,
-		{description = 'decrease floating client size horizontally by 10 px left', group = 'client'}
-	),
-	awful.key(
-		{modkey, 'Control'},
-		'Right',
-		function(c)
-			if c.floating then
-				local c_width = c.width
-				c:relative_move(0, 0, dpi(-10), 0)
-				if c.width ~= c_width and c.width > 10 then
-					c:relative_move(dpi(10), 0 , 0, 0)
-				end
-			end
-		end,
-		{description = 'decrease floating client size horizontally by 10 px right', group = 'client'}
-	)
+	-- awful.key(
+	-- 	{modkey, 'Control'},
+	-- 	'Up',
+	-- 	function(c)
+	-- 		if c.floating and c.height > 10 then
+	-- 			c:relative_move(0, 0, 0, dpi(-10))
+	-- 		end
+	-- 	end,
+	-- 	{description = 'decrease floating client size vertically by 10 px up', group = 'client'}
+	-- ),
+	-- awful.key(
+	-- 	{modkey, 'Control'},
+	-- 	'Down',
+	-- 	function(c)
+	-- 		if c.floating then
+	-- 			local c_height = c.height
+	-- 			c:relative_move(0, 0, 0, dpi(-10))
+	-- 			if c.height ~= c_height and c.height > 10 then
+	-- 				c:relative_move(0, dpi(10), 0, 0)
+	-- 			end
+	-- 		end
+	-- 	end,
+	-- 	{description = 'decrease floating client size vertically by 10 px down', group = 'client'}
+	-- ),
+	-- awful.key(
+	-- 	{modkey, 'Control'},
+	-- 	'Left',
+	-- 	function(c)
+	-- 		if c.floating and c.width > 10 then
+	-- 			c:relative_move(0, 0, dpi(-10), 0)
+	-- 		end
+	-- 	end,
+	-- 	{description = 'decrease floating client size horizontally by 10 px left', group = 'client'}
+	-- ),
+	-- awful.key(
+	-- 	{modkey, 'Control'},
+	-- 	'Right',
+	-- 	function(c)
+	-- 		if c.floating then
+	-- 			local c_width = c.width
+	-- 			c:relative_move(0, 0, dpi(-10), 0)
+	-- 			if c.width ~= c_width and c.width > 10 then
+	-- 				c:relative_move(dpi(10), 0 , 0, 0)
+	-- 			end
+	-- 		end
+	-- 	end,
+	-- 	{description = 'decrease floating client size horizontally by 10 px right', group = 'client'}
+	-- )
 )
 
 return clientKeys
