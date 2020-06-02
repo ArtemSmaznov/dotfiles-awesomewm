f15fe9b770e4d4bad586695a812b187af4ada64d
Artem Smaznov <ex.black@gmail.com>
2020-06-02 19:44:37 +0100

    awesome/fix screenshot

--- config/awesome/floppy/module/screenshot.lua
+++ config/awesome/floppy/module/screenshot.lua
@@ -3,65 +3,67 @@ local awful = require('awful')
 
 return function(mode)
 	local screenshot_dir = os.getenv('HOME') .. '/Pictures/Screenshots/'
-	local notif_message = 'Screenshot Captured!'
-	local file_loc = screenshot_dir .. os.date("%Y-%m-%d_%H-%M-%S") .. '.png'
-	-- local file_loc = screenshot_dir .. os.date("%Y-%m-%d_%T") .. '.png'
+	local file_loc = screenshot_dir .. os.date("%Y-%m-%d_%T") .. '.png'
+	local maim_command, notif_message
 
 	if (mode == 'full') then
-		awful.spawn('maim -u -m 1 '.. file_loc)
+		maim_command = 'maim -u -m 1 '
 		notif_message = "Full screenshot saved and copied to clipboard!"
 
 	elseif (mode == 'window') then
-		awful.spawn.with_shell('maim -u -B -i $(xdotool getactivewindow) -m 1 '.. file_loc)
+		maim_command = 'maim -u -B -i $(xdotool getactivewindow) -m 1 '
 		notif_message = "Current window screenshot saved and copied to clipboard!"
 
 	elseif (mode == 'area') then
-		awful.spawn('maim -u -s -n -m 1 '.. file_loc)
+		maim_command = 'maim -u -s -n -m 1 '
 		notif_message = "Area screenshot saved and copied to clipboard!"
 
 	else
 		notif_message = "Wrong Argument Used!"
 	end
 
-	awful.spawn('xclip -selection clipboard -t image/png -i ' .. file_loc)
+	awful.spawn.easy_async_with_shell(maim_command .. file_loc, function()
 
-	local open_image = naughty.action {
-		name = 'Open',
-			 icon_only = false,
-	}
+		awful.spawn('xclip -selection clipboard -t image/png -i ' .. file_loc)
 
-	local open_folder = naughty.action {
-		name = 'Folder',
-			 icon_only = false,
-	}
+		local open_image = naughty.action {
+			name = 'Open',
+				icon_only = false,
+		}
 
-	local delete_image = naughty.action {
-		name = 'Delete',
-			 icon_only = false,
-	}
+		local open_folder = naughty.action {
+			name = 'Folder',
+				icon_only = false,
+		}
 
-	-- Execute the callback when 'Open' is pressed
-	open_image:connect_signal('invoked', function()
-		awful.spawn('xdg-open ' .. file_loc, false)
+		local delete_image = naughty.action {
+			name = 'Delete',
+				icon_only = false,
+		}
+
+		-- Execute the callback when 'Open' is pressed
+		open_image:connect_signal('invoked', function()
+			awful.spawn('xdg-open ' .. file_loc, false)
+		end)
+
+		open_folder:connect_signal('invoked', function()
+			awful.spawn('xdg-open ' .. screenshot_dir, false)
+		end)
+
+		-- Execute the callback when 'Delete' is pressed
+		delete_image:connect_signal('invoked', function()
+			awful.spawn('gio trash ' .. file_loc, false)
+		end)
+
+		-- Show notification
+		naughty.notification ({
+			app_name = 'Screenshot Tool',
+			icon = file_loc,
+			timeout = 10,
+			title = '<b>Snap!</b>',
+			message = notif_message,
+			actions = { open_image, open_folder, delete_image }
+		})
 	end)
-
-	open_folder:connect_signal('invoked', function()
-		awful.spawn('xdg-open ' .. screenshot_dir, false)
-	end)
-
-	-- Execute the callback when 'Delete' is pressed
-	delete_image:connect_signal('invoked', function()
-		awful.spawn('gio trash ' .. file_loc, false)
-	end)
-
-	-- Show notification
-	naughty.notification ({
-		app_name = 'Screenshot Tool',
-		icon = file_loc,
-		timeout = 10,
-		title = '<b>Snap!</b>',
-		message = notif_message,
-		actions = { open_image, open_folder, delete_image }
-	})
 
 end
