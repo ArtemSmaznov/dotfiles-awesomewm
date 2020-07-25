8557df3cf77772cd7811b4991fd6fd1ca9066cca
Artem Smaznov <ex.black@gmail.com>
2020-07-26 00:32:15 +0100

    awesome/get wifi interface

--- config/awesome/module/wifi.lua
+++ config/awesome/module/wifi.lua
@@ -1,6 +1,13 @@
 local awful = require('awful')
 
-local wlan_interface = 'wlp0s20u10'
+-- local wlan_interface = 'wlp0s20u10'
+local wlan_interface
+awful.spawn.easy_async_with_shell(
+  "iw dev | grep Interface | awk '{print $2}'",
+  function(stdout)
+    wlan_interface = stdout
+  end
+)
 
 local toggle_state
 
